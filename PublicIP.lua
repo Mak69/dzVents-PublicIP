49553bbc76d81535feff9a40987235ba592886af
Mak69 <marco.cogo@outlook.it>
2018-01-13 14:42:41 +0100

    Second commit

--- PublicIP.lua
+++ PublicIP.lua
@@ -1,3 +1,4 @@
+
 return {
     active = true,
     on = {
@@ -5,27 +6,26 @@ return {
     },
     data = {
             lastIP = { initial = '0.0.0.0' },
-            WAN_connected = false
+            WAN_connected = { initial = false }
     },
     execute = function(domoticz)
             local Host = "https://api.ipify.org"
             local ErrMsg = "WAN connection lost"
-            local f = io.popen("curl -s " .. Host , 'r')
-            if (f == nil) 
-                domoticz.log ('[Error] - io.open failed')
-                local s = assert(f:read('*a'))
-            else
-                local s = "0"
+            local f = assert(io.popen("curl -s " .. Host , 'r'))
+            local s = assert(f:read('*a'))
+            if (s == nil) then
+                s = "0"
             end
+
             domoticz.log ('Last   IP: ' .. domoticz.data.lastIP)
             domoticz.log ('Actual IP: ' .. s)
-            if (s == "0" and WAN_connected) then 
+            if (s == '0' and domoticz.data.WAN_connected == true) then
                 domoticz.data.WAN_connected = false
                 domoticz.log ("Unable to contact " .. Host)
                 domoticz.variables('PublicIP').set(ErrMsg)
-                domoticz.devices('PublicIP').updateText(ErrMsg)
-                domoticz.data.lastIP = "0"
-            elseif (domoticz.data.lastIP ~= s) then    
+                domoticz.devices('Public IP').updateText(ErrMsg)
+                domoticz.data.lastIP = '0'
+            elseif (domoticz.data.lastIP ~= s) then
                 domoticz.data.WAN_connected = true
                 domoticz.log ('Changing device value -> ' .. s);
                 domoticz.variables('PublicIP').set(s)
